5b608c697c434e0c24a92562368c7abe43d9426e
v38armageddon <69727502+v38armageddon@users.noreply.github.com>
2022-08-02 22:39:34 +0200

    update

---
+++ mcpy-tools/index.py
@@ -0,0 +1,70 @@
+import digging
+import canplacedestroy
+
+import debugmode
+from debugmode import *
+
+import strongholdfinder
+import level
+import chunkcal
+import wall
+
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+
+
+bm = Back.MAGENTA
+g = Fore.GREEN
+b = Fore.BLUE
+c = Fore.CYAN
+w = Fore.WHITE
+
+def menu():
+    print(b + "- - - - - MCPy Tools - - - - -")
+    print(b + "- - - - -     By     - - - - -")
+    print(b + "- - - - -  ZeyaTsu   - - - - -")
+
+    print(c + "- - - - - Choose an App - - - - -") 
+    print(b + "1. Area Block Counter - Count blocks from a given area.") # check
+    print(b + "2. Mineshaft mirror - Find another mineshaft by finding a mineshaft.")
+    print(b + "3. CanPlaceOn&CanDestroy - Command generator to give item that can destroy/be place on specific block.") # check
+    print(b + "4. Stronghold Finder - Find stronghold, require 2 Ender eye.")# check
+    print(b + "5. Best Y Level - List of all best Y levels for ores.")# check
+    print(b + "6. Blocks/Chunk converter - Conert chunks to blocks & vice versa.")# check
+    print(b + "7. Perimeter Calculation")
+
+    print(c + "Write 'list' to open this list.")
+    
+    s = True
+    while s:
+        choice = str(input("> "))
+        if choice == "list":
+            menu()
+        if choice == "debug":
+            print(bm + w +"Debug Mode activated. Write 's-d' to stop debugging."+ Back.RESET, c)
+            debugmode.debug = True
+        if choice == "s-d":
+            debugmode.debug = False
+
+
+        elif choice == "1":
+            digging.setpos()
+            break
+        elif choice == "3":
+            canplacedestroy.choix()
+            break
+        elif choice == "4":
+            strongholdfinder.all()
+            break
+        elif choice == "5":
+            level.y()
+            break
+        elif choice == "6":
+            chunkcal.cc()
+            break
+        elif choice == "7":
+            wall.perimeter()
+            break
+
+         
---
+++ mcpy-tools/strongholdfinder.py
@@ -0,0 +1,113 @@
+import colorama
+from colorama import Fore, Back, Style
+import index
+import debugmode
+from debugmode import *
+
+
+colorama.init()
+bm = Back.MAGENTA
+w = Fore.WHITE
+g = Fore.GREEN
+c = Fore.CYAN
+import math
+
+def all():
+
+
+    print(g+"- - - - - Stronghold Finder - - - - -")
+
+    print(g+"Follow the steps to get the correct coordinates.")
+    print("1. Go to X:0 and Z:0 (Press F3 to show coordinates)")
+    print("2. Throw an ender eye, look at the center of it and look at 'Facing' Section.")
+    print("Write down the Facing (North,south,west or east) and the first number after it.")
+    print("E.g: Facing: north (Towars negative Z) (xxx <- this number)")
+    print("Write that number here. (The most you look to the center of it, the most accurate it is.)")
+    ThisAngle1 = float(input("> "))
+    facing = str(input("Facing: north/south/east/west > "))
+
+    if facing == "north":
+        print("Please go to X:0 Z: -310")
+        c = -310
+    if facing == "south":
+        print("Please go to X:0 Z: 310")
+        c = 310
+    if facing == "west":
+        print("Please go to X:-310 Z:0")
+        c = -310
+    if facing == "east":
+        print("Please go to X:310 Z:0")
+        c = 310
+    print("Redo the steps and enter the number you get in the facing section:")
+    ThisAngle2 = float(input("> "))
+
+    
+
+#142
+#135.1
+    angle1 = ThisAngle1
+    angle2 = ThisAngle2
+    h0 = 90 - ThisAngle1
+    h00 = 90 - ThisAngle2
+
+    def north(h0, c, h00):
+        h1 = math.radians(h0)
+        h2 = math.radians(h00)
+       
+
+        xNorthFind = -(c / (math.tan(h1) - math.tan(h2)))
+        zNorthFind = (c*math.tan(h1)) / (math.tan(h1) - math.tan(h2)) 
+
+        print("- - - - -  Coordinates - - - - -")
+
+        print("Stronghold found!")
+        print("X:",xNorthFind, "Z:",zNorthFind)
+        print("Note: If you don't find the stronghold on those coordinates, it may be around of that area.")
+        print("You can use an cave finder bug if you have troubles.")
+        choix()
+
+
+
+       
+    def west(c, angle1, angle2):
+        print("gfhjghdfjgd")
+        h3 = math.radians(angle1)
+        h4 = math.radians(angle2)
+        aWestFind = (c * math.tan(h3)) / (math.tan(h3) - math.tan(h4))
+        bWestFind = -(c / (math.tan(h3) - math.tan(h4)))
+
+        print("- - - - -  Coordinates - - - - -")
+
+        print("Stronghold found!")
+        print("X:",aWestFind, "Z:",bWestFind)
+        print("Note: If you don't find the stronghold on those coordinates, it may be around of that area.")
+        print("You can use an cave finder bug if you have troubles.")
+        choix()
+
+
+    if facing == "west":
+        west(c, angle1, angle2)
+    if facing == "east":
+        west(c, angle1, angle2)
+    if facing == "north":
+        north(h0, c, h00)
+    if facing == "south":
+        north(h0, c, h00)
+
+    
+ 
+
+
+    
+def choix():
+    print(c+"Write 'list' to go back. 'r' to restart.")
+    w = True
+    while w:
+        
+        ca = str(input("> "))
+        if ca == "list":
+            index.menu()
+            w = False
+        elif ca == "r" or "res" or "restart":
+            all()
+            w = False
